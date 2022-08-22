abfb4a5b18aa69913c0e5c34055c373b1b325744
Sergei-Eremin <serezhanv@gmail.com>
2022-08-22 18:11:32 +0300

    feat: library formation

--- src/components/game/game.ts
+++ src/components/game/game.ts
@@ -7,26 +7,30 @@ export default class Game{
         return document.location.pathname.includes('sprint') ? "sprint" : "audio-call";
     }
 
-    checkParameter(): undefined | IapiRequestWords{
-        if(document.location.search !== ""){
-            const params = new URLSearchParams(document.location.search);
-            const page = params.get('page') as string;
-            const group = params.get('group') as string;
-            return {page, group}
-        } 
-        return undefined
+    checkParameter(): IapiRequestWords | undefined{
+        if(document.location.search === "") return undefined
+        
+        const params = new URLSearchParams(document.location.search);
+        const page = params.get('page') as string;
+        const group = params.get('group') as string;
+        return {page, group}
     }
 
-    createLibrary(){
+    async createLibrary(){
         const gameName = this.checkGameName();
+        console.log(gameName, "gameName");
+        
         const parameterPage = this.checkParameter();
         const realPage = String(Number(parameterPage?.page) - 1);
+        const page = Number(parameterPage?.page);
+        console.log(parameterPage, "parameterPage");
+        console.log(realPage, "gamerealPageName");
+        console.log(page, "page");
         // getWords()
         if(gameName === "sprint"){
-            if(realPage >= "1" && parameterPage !== undefined){
-                // return getWords(parameterPage)
-                console.log(getWords(parameterPage));
-                
+            if(Number(page) >= 1 && parameterPage !== undefined){
+                console.log(await getWords(parameterPage), 'библиотека слов');
+                return await getWords(parameterPage);
             }
         }
         else if(gameName === "audio-call"){
