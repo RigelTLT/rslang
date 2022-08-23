759ae9d757807dab63ed50a7e7757a8c05b3a426
Oleg Somov <gog0125@mail.ru>
2022-08-23 18:04:07 +0300

    feat: create timer and fillWords methods  in class Sprint

--- src/components/sprint/sprint.ts
+++ src/components/sprint/sprint.ts
@@ -8,6 +8,42 @@ export class Sprint {
     const game = new Game();
     this.library = (await game.createLibrary()) as ILibraryResponse[];
     game.renderTemplate(game.checkGameName());
+    this.timer();
+    this.fillWord(1, false);
+  }
+
+  timer() {
+    const timerSelect = document.querySelector('.timer') as HTMLDivElement;
+    let timeLeft = Number(timerSelect.textContent);
+
+    const intervalId = setInterval(() => {
+      timeLeft = timeLeft - 1;
+      timerSelect.innerText = timeLeft.toString();
+
+      if (timeLeft < 1) clearInterval(intervalId);
+    }, 1000);
+
+    return timeLeft;
+  }
+
+  fillWord(wordId: number, isRightTranslate: boolean) {
+    const [originalWord, translateWord] = (document.querySelector('.words') as HTMLDivElement).children;
+
+    if (isRightTranslate) {
+      originalWord.innerHTML = this.library[wordId].word;
+      translateWord.innerHTML = this.library[wordId].wordTranslate;
+    } else {
+      originalWord.innerHTML = this.library[wordId].word;
+      let randomIndex = this.randomIndexGenerator(this.library.length);
+
+      if (randomIndex === wordId) randomIndex = this.randomIndexGenerator(this.library.length);
+
+      translateWord.innerHTML = this.library[randomIndex].wordTranslate;
+    }
+  }
+
+  randomIndexGenerator(maxLength: number) {
+    return Math.floor(Math.random() * maxLength);
   }
 }
 
