80364d9efcb95ed41b77f7d0acd16e580713307b
Oleg Somov <gog0125@mail.ru>
2022-08-23 20:19:42 +0300

    feat: add btnListener, randomBooleanGenerator methods

--- src/components/sprint/sprint.ts
+++ src/components/sprint/sprint.ts
@@ -4,12 +4,18 @@ import { ILibraryResponse } from '../interface/interface';
 export class Sprint {
   library: ILibraryResponse[] = [];
 
+  selectedWordIndex = 0;
+
   async start() {
     const game = new Game();
+
     this.library = (await game.createLibrary()) as ILibraryResponse[];
     game.renderTemplate(game.checkGameName());
     this.timer();
-    this.fillWord(1, false);
+    this.fillWord(this.selectedWordIndex, this.randomBooleanGenerator());
+
+    this.trueButtonHandler();
+    this.falseButtonHandler();
   }
 
   timer() {
@@ -34,16 +40,48 @@ export class Sprint {
       translateWord.innerHTML = this.library[wordId].wordTranslate;
     } else {
       originalWord.innerHTML = this.library[wordId].word;
-      let randomIndex = this.randomIndexGenerator(this.library.length);
-
-      if (randomIndex === wordId) randomIndex = this.randomIndexGenerator(this.library.length);
+      const randomIndex = this.randomIndexGenerator(this.library.length, wordId);
 
       translateWord.innerHTML = this.library[randomIndex].wordTranslate;
     }
   }
 
-  randomIndexGenerator(maxLength: number) {
-    return Math.floor(Math.random() * maxLength);
+  btnListener = () => {
+    this.selectedWordIndex++;
+
+    if (this.selectedWordIndex >= this.library.length) return alert('слова кончились');
+
+    this.fillWord(this.selectedWordIndex, this.randomBooleanGenerator());
+  };
+
+  trueButtonHandler() {
+    // const falseButton = document.querySelector('.control .false');
+    const trueButton = document.querySelector('.control .true');
+
+    trueButton?.addEventListener('click', this.btnListener);
+    document.body?.addEventListener('keyup', (event) => event.key === 'ArrowLeft' && this.btnListener());
+  }
+
+  falseButtonHandler() {
+    const falseButton = document.querySelector('.control .false');
+    // const trueButton = document.querySelector('.control .true');
+
+    falseButton?.addEventListener('click', this.btnListener);
+    document.body.addEventListener('keyup', (event) => event.key === 'ArrowRight' && this.btnListener());
+  }
+
+  randomBooleanGenerator() {
+    return Math.random() > 0.5 ? true : false;
+  }
+
+  randomIndexGenerator(maxLength: number, exclude: number) {
+    const randomNubmer = Math.floor(Math.random() * maxLength);
+
+    if (randomNubmer === exclude) {
+      this.randomIndexGenerator(maxLength, exclude);
+    }
+
+    return randomNubmer;
   }
 }
 
