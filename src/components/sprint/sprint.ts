378864f9594e447618859132f8c8a816b9aec6d7
Sergei-Eremin <serezhanv@gmail.com>
2022-08-26 14:58:54 +0300

    fix: fix point link

--- src/components/sprint/sprint.ts
+++ src/components/sprint/sprint.ts
@@ -15,12 +15,12 @@ export class Sprint {
   wrongWordArr: ILibraryResponse[] = [];
 
   //
-  points = document.querySelector('.points') as HTMLElement;
-  //
 
   private _bool = () => (Math.random() > 0.5 ? true : false);
 
   get boolean(): boolean {
+    console.log(this._bool(), 'результат геттера boolean');
+
     return this._bool();
   }
 
@@ -29,6 +29,8 @@ export class Sprint {
 
     this.library = (await game.createLibrary()) as ILibraryResponse[];
     game.renderTemplate(game.checkGameName());
+    console.log(this.boolean, 'boolean при старте');
+
     this.fillWord(this.selectedWordIndex, this.boolean);
 
     this.trueButtonHandler();
@@ -52,12 +54,14 @@ export class Sprint {
 
   fillWord(wordId: number, isRightTranslate: boolean) {
     const [originalWord, translateWord] = (document.querySelector('.words') as HTMLDivElement).children;
+    console.log(isRightTranslate, 'isRightTranslate в fillWord');
 
     if (isRightTranslate) {
       originalWord.innerHTML = this.library[wordId].word;
       translateWord.innerHTML = this.library[wordId].wordTranslate;
     } else {
       originalWord.innerHTML = this.library[wordId].word;
+      // console.log(this.randomIndexGenerator(this.library.length, wordId), 'при фолсе');
       const randomIndex = this.randomIndexGenerator(this.library.length, wordId);
 
       translateWord.innerHTML = this.library[randomIndex].wordTranslate;
@@ -68,10 +72,14 @@ export class Sprint {
     const trueButton = document.querySelector('.control .true') as HTMLElement;
 
     const btnListener = () => {
+      const points = document.querySelector('.points') as HTMLElement;
       const translateWord = (document.querySelector('.words .translate') as HTMLDivElement).textContent;
       const nextIndex = this.selectedWordIndex + 1;
 
       const isRightTranslate = this.boolean;
+      console.log('нажатие на true');
+
+      console.log(isRightTranslate, 'isRightTranslate в кнопке true');
       if (nextIndex < this.library.length) {
         this.fillWord(nextIndex, isRightTranslate);
       }
@@ -79,7 +87,7 @@ export class Sprint {
       if (this.library[this.selectedWordIndex].wordTranslate === translateWord) {
         this.countRight += 1;
         this.resultScores += 20;
-        this.points.textContent = String(this.resultScores);
+        points.textContent = String(this.resultScores);
         this.rightWordsArr.push(this.library[this.selectedWordIndex]);
       } else {
         this.countRight = 0;
@@ -100,10 +108,13 @@ export class Sprint {
     const falseButton = document.querySelector('.control .false') as HTMLElement;
 
     const btnListener = () => {
+      const points = document.querySelector('.points') as HTMLElement;
       const translateWord = (document.querySelector('.words .translate') as HTMLDivElement).textContent;
       const nextIndex = this.selectedWordIndex + 1;
 
       const isRightTranslate = this.boolean;
+      console.log('нажатие на false');
+      console.log(isRightTranslate, 'isRightTranslate в кнопке false');
       if (nextIndex < this.library.length) {
         this.fillWord(nextIndex, isRightTranslate);
       }
@@ -111,7 +122,7 @@ export class Sprint {
       if (this.library[this.selectedWordIndex].wordTranslate !== translateWord) {
         this.countRight += 1;
         this.resultScores += 20;
-        this.points.textContent = String(this.resultScores);
+        points.textContent = String(this.resultScores);
         this.rightWordsArr.push(this.library[this.selectedWordIndex]);
       } else {
         this.countRight = 0;
@@ -129,13 +140,16 @@ export class Sprint {
   }
 
   randomIndexGenerator(maxLength: number, exclude: number): number {
-    const randomNubmer = Math.floor(Math.random() * maxLength - 1);
-
-    if (randomNubmer === exclude) {
-      return this.randomIndexGenerator(maxLength - 1, exclude);
+    const randomNumber = Math.floor(Math.random() * maxLength);
+    if (!randomNumber) {
+      return this.randomIndexGenerator(maxLength, exclude);
     }
 
-    return randomNubmer;
+    if (randomNumber === exclude) {
+      return this.randomIndexGenerator(maxLength, exclude);
+    }
+
+    return randomNumber;
   }
 }
 
