65ed54d6ae49fa358380220e87321f21a0b34d29
Sergei-Eremin <serezhanv@gmail.com>
2022-09-03 12:31:20 +0300

    feat: basic statistic template

---
+++ src/components/statistic/statistic.ts
@@ -0,0 +1,10 @@
+import './statistic.scss';
+import Game from '../game/game';
+
+class Statistic {
+  start() {
+    const game = new Game();
+    // const gamePapametr = game.checkParameter();
+  }
+}
+new Statistic().start();
